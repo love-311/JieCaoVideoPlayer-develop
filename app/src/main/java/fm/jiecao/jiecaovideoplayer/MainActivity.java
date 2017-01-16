113b94a06fe020bf0fed775352db99becac54a75
love_311 <13628180947@qq.com>
2017-01-16 16:46:36 +0800

    compile 'com.google.android.gms:play-services:10.0.1'

--- app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAboutWebView.setOnClickListener(this);
 
         mJcVideoPlayerStandard = (JCVideoPlayerStandard) findViewById(R.id.jc_video);
-        mJcVideoPlayerStandard.setUp("http://video.jiecao.fm/11/23/xin/%E5%81%87%E4%BA%BA.mp4"
+        mJcVideoPlayerStandard.setUp("http://120.76.191.63/net/./uploads/2017-01-13/file.mp4"
                 , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子不信");
         Picasso.with(this)
                 .load("http://img4.jiecaojingxuan.com/2016/11/23/00b026e7-b830-4994-bc87-38f4033806a6.jpg@!640_360")
